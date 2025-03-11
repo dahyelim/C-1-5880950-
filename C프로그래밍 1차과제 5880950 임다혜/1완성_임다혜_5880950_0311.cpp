6a81bac16546a88c61fc66983766cdd12d24d36e
dahyelim <lim3016566@naver.com>
2025-03-11 15:46:51 +0900

    과제

--- C프로그래밍 1차과제 5880950 임다혜/1완성_임다혜_5880950_0311.cpp
+++ C프로그래밍 1차과제 5880950 임다혜/1완성_임다혜_5880950_0311.cpp
@@ -1,13 +1,12 @@
-#define _CRT_SECURE_NO_WARNINGS
-#include<stdio.h>
+#define _CRT_SECURE_NO_WARNINGS //scanf,printf 함수 사용시 발생하는 보안경고 비활성화
+#include<stdio.h>// 표준 입출력 함수 라이브러리
 
 int main() {
-	int num = 0;
-	printf("정수를 입력하세요 : ");
-	scanf("%d", &num);
-	for (int i = 0; i < 32; ++i) {
-		if (i % 4 == 0 && i != 0)
-			putchar(' ');
+	int num = 0; //입력받을 변수 num선언
+	printf("정수를 입력하세요 : ");//메시지 출력
+	scanf("%d", &num);//정수 입력받기
+	for (int i = 0; i < 32; ++i) {//32번 반복문
+		//num의 각 비트를 차례로 확인하고 출력
 		if (num & (1 << (31 - i)))
 			putchar('1');
 		else
